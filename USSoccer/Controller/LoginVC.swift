48f9dab399a33f1da6774df759223d8b62a73bf9
Jareedos <jaredallens@gmail.com>
2017-10-04 19:32:26 -0700

    Added settup for loginVC

---
+++ USSoccer/Controller/LoginVC.swift
@@ -0,0 +1,46 @@
+//
+//  LoginVC.swift
+//  USSoccer
+//
+//  Created by Jared Sobol on 10/4/17.
+//  Copyright © 2017 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTxtField: UITextField!
+
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginBtnClicked(_ sender: Any) {
+    }
+    
+    @IBAction func fogotPasswordBtnClicked(_ sender: Any) {
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ USSoccer/Model/SoccerGame.swift
@@ -0,0 +1,15 @@
+//
+//  SoccerGame.swift
+//  USSoccer
+//
+//  Created by Jared Sobol on 10/4/17.
+//  Copyright © 2017 Appmaker. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+class SoccerGame {
+    
+}
