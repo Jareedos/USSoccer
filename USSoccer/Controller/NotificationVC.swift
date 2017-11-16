b32f3078530fec533bd9966ee5aed464f8619325
Jareedos <jaredallens@gmail.com>
2017-11-15 21:10:55 -0800

    add bellIcon and button in TableViewCells

---
+++ USSoccer/Controller/NotificationVC.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationVC.swift
+//  USSoccer
+//
+//  Created by Jared Sobol on 11/15/17.
+//  Copyright © 2017 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class NotificationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func cancelBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
