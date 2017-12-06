1fd76e288a3ea630db504163330a86f5d24c3055
Jareedos <jaredallens@gmail.com>
2017-12-05 20:52:04 -0800

    Updated Project to Intergrate One Signal Correctly

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
- platform :ios, ’11.0’
+ platform :ios, '11.0'
 
 target 'USSoccer' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
@@ -7,8 +7,9 @@ target 'USSoccer' do
 
   # Pods for USSoccer
 pod 'Alamofire', '~> 4.5'
+pod 'OneSignal', '>= 2.5.2', '< 3.0'
 pod 'Firebase/Core'
 pod 'Firebase/Auth'
 pod 'Firebase/Database'
-
 end
+
--- USSoccer/Utilities/AppDelegate.swift
+++ USSoccer/Utilities/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import CoreData
+import OneSignal
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        let onesignalInitSettings = [kOSSettingsKeyAutoPrompt: false]
+        
+        // Replace 'YOUR_APP_ID' with your OneSignal App ID.
+        OneSignal.initWithLaunchOptions(launchOptions,
+                                        appId: "YOUR_APP_ID",
+                                        handleNotificationAction: nil,
+                                        settings: onesignalInitSettings)
+        
+        OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification;
+        
+        // Recommend moving the below line to prompt for push after informing the user about
+        //   how your app will use them.
+        OneSignal.promptForPushNotifications(userResponse: { accepted in
+            print("User accepted notifications: \(accepted)")
+        })
         return true
     }
 
